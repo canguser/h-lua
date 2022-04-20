eec51d2be2c7b075d4e773f2b2ade9a4986410f7
hunzsig <mzyhaohaoren@qq.com>
2022-04-20 23:53:35 +0800

    chore(sdk): help

--- depend/sdk/cmd/help.go
+++ depend/sdk/cmd/help.go
@@ -6,7 +6,8 @@ import (
 
 func Help() {
 	fmt.Println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
-	fmt.Println("┃ *:required / ~:optional")
+	fmt.Println("┃ *:required")
+	fmt.Println("┃ ~:optional")
 	fmt.Println("┃ sdk.exe new [:PROJECT_NAME] - new a project")
 	fmt.Println("┃ sdk.exe we [:PROJECT_NAME] - open map by YDWE")
 	fmt.Println("┃ sdk.exe model [*PROJECT_NAME] [~CURRENT_PAGE:0] - view models, 289 in a page")
@@ -16,5 +17,7 @@ func Help() {
 	fmt.Println("┃ sdk.exe test [:PROJECT_NAME] - test a project")
 	fmt.Println("┃ sdk.exe build [:PROJECT_NAME] - package a project for pre launch")
 	fmt.Println("┃ sdk.exe dist [:PROJECT_NAME] - package a project for launch")
+	fmt.Println("┃")
+	fmt.Println("┃ QQ:325338043 Pwd:HJux213jP")
 	fmt.Println("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
 }
